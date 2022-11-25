6bc0338e8078e3b07912ac128f0498db6bc17c59
unknown <syabdoul100@gmail.com>
2022-11-25 16:41:04 +0000

    exporting the project to github

---
+++ index.js
@@ -0,0 +1,27 @@
+const advicePlaceholder = document.querySelector('.advice');
+const adviceId = document.querySelector('.adviceId');
+const btn = document.querySelector('.btn');
+
+addEventListener('load', () => {
+    getAdvice()
+});
+
+btn.addEventListener('click', () => {
+    getAdvice()
+});
+
+
+function getAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(res =>   res.json()
+    )
+    .then(data => {
+        const adviceObject = data.slip;
+        advicePlaceholder.innerHTML = adviceObject.advice;
+        adviceId.innerHTML= `#${adviceObject.id}`;
+    }
+    )
+    .catch(error => console.log(error))
+}
+
+
