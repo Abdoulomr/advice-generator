0a2cc5bc4848ca220139e4dc44ab9ba20e0e05d7
unknown <syabdoul100@gmail.com>
2023-07-12 16:03:01 +0000

    code refresh

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const advicePlaceholder = document.querySelector('.advice');
-const adviceId = document.querySelector('.adviceId');
-const btn = document.querySelector('.btn');
+const advicePlaceholder = document.querySelector(".advice");
+const adviceId = document.querySelector(".adviceId");
+const btn = document.querySelector(".btn");
 
-addEventListener('load', () => {
-    getAdvice()
+addEventListener("load", () => {
+  getAdvice();
 });
 
-btn.addEventListener('click', () => {
-    getAdvice()
+btn.addEventListener("click", () => {
+  getAdvice();
 });
-
 
 function getAdvice() {
-    fetch('https://api.adviceslip.com/advice')
-    .then(res =>   res.json()
-    )
-    .then(data => {
-        const adviceObject = data.slip;
-        advicePlaceholder.innerHTML = adviceObject.advice;
-        adviceId.innerHTML= `#${adviceObject.id}`;
-    }
-    )
-    .catch(error => console.log(error))
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      const adviceObject = data.slip;
+      advicePlaceholder.innerHTML = adviceObject.advice;
+      adviceId.innerHTML = `#${adviceObject.id}`;
+    })
+    .catch((error) => console.log(error));
 }
-
-
